9dcf172752599ba2cd521b589671d94aa051fbdf
XAdriaa <s.adria.navarro@gmail.com>
2025-12-09 12:06:27 +0100

    Inicio sesion 2

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', function () {
-    return "Listado de posts";
-}); 
+Route::get('/posts/{id}', function ($id = "9") {
+    return "Listado de posts, " . $id;
+}) -> Where('id' . "[0-9]*");
+
+//Ruta simple
+Route::get('fecha', function () {
+    return date("d/m/y h:i:s");
+});
+
+//Ruta con parametros
+Route::get('saluda/{nombre?}', function ($nombre = "Messi"){
+    return "Hola, " . $nombre;
+}) ->Where('nombre', "[A-Za-z]+");
